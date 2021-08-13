50ab834c02909c364589fe33a9c5255ca653b84c
JordanMcDonald13 <Jordanm.mcdonald13@gmail.com>
2021-08-12 23:07:21 -0500

    fade

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 console.log("your index is loaded correctly");
 
 $("#toggle").on("click", function(){
-    $("#dropDown").slideDown();
+    $("#dropDown").fadeToggle();
   });
 
   $("#toggle1").on("click", function(){
-    $("#dropDown1").slideDown();
+    $("#dropDown1").fadeToggle();
   });
 
   $("#toggle2").on("click", function(){
-    $("#dropDown2").slideDown();
+    $("#dropDown2").fadeToggle();
   });
 
   $("#toggle3").on("click", function(){
-    $("#dropDown3").slideDown();
+    $("#dropDown3").fadeToggle();
   });
